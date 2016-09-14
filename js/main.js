c77c6d938128d013bf6abb829a8e132cc9aea5c4
Bin Liu <bin.w.liu+1@gmail.com>
2016-09-14 16:38:40 -0400

    Add more detail to Read me file, add index and main.js for starter

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
+var GameState = {
+	preload: function(){
+
+	},
+	create: function(){
+
+	},
+	update: function(){
+		
+	}
+};
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
