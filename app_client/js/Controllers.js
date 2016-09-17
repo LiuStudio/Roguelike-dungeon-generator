7305266c92f8219a5a9b663f9b95bb98293a4fc3
Bin Liu <bin.w.liu@gmail.com>
2016-09-16 22:31:04 -0400

    Final submission
    beautify display, Add Map Entrance, Remove DeadEnd function, Add Legend

--- app_client/js/Controllers.js
+++ app_client/js/Controllers.js
@@ -5,22 +5,18 @@ angular.module('Dungeon-Generator-App')
 		$scope.numberroom = '';
 		var generateMap = function(){
 			MapGenService.map_init();
-			MapGenService.generateRooms(2);
+			MapGenService.generateRooms(8);
 			MapGenService.generateAllPaths();
 			MapGenService.connectMaze();
+			
+		    MapGenService.removeDeadEnds();
+		    MapGenService.AddEntAndExit();
 		    $scope.map_object = MapGenService.map_toJSON();	
 		}
 		
 
 		$scope.generateMap = generateMap;
 		
-
-		$scope.testobj = {
-			rows: [{data: [1,2,3]},
-					{data:[4,5,6]},
-					{data:[7,8,9]}	]
-		};
-
 	}])
 	
 	.controller('NavController', ['$scope', function($scope){
--- app_client/js/map.js
+++ app_client/js/map.js
@@ -1,8 +1,8 @@
 angular.module('Dungeon-Generator-App')
 	.service('MapGenService',function(){
-		const MAP_X = 10;
-		const MAP_Y = 10;
-		const ROOM_SIZE_MAX = 5;
+		const MAP_X = 30;
+		const MAP_Y = 30;
+		const ROOM_SIZE_MAX = 8;
 		const ROOM_SIZE_MIN = 3;
 		const PIX_SIZE = 5;
 		var floor_map = [];
@@ -10,7 +10,7 @@ angular.module('Dungeon-Generator-App')
 		var paths= [];
 		//x and y are zero based, align with array index
 		var Tile = function Tile(x,y){
-			this.floorType = 0; //0 -- floor, 1--room , 2--pathway, 3--entrance
+			this.floorType = 0; //0 -- floor, 1--room , 2--pathway, 3--entrance , 4--Map Entrance, 5--Map Exit
 			this.connected = 0; // at the end, any tile which is not floor, should be connected
 			this.X = x;
 			this.Y = y;
@@ -51,6 +51,14 @@ angular.module('Dungeon-Generator-App')
 		};
 
 
+		Tile.prototype.isMapEntrance = function() {
+			return (this.floorType == 4);
+		};
+
+		Tile.prototype.isMapExit = function() {
+			return (this.floorType == 5);
+		};
+
 		Tile.prototype.isRoom = function() {
 			return (this.floorType == 1);
 		};
@@ -77,8 +85,17 @@ angular.module('Dungeon-Generator-App')
 			this.floorType = 3;
 		};
 
+		Tile.prototype.toMapEntrance = function() {
+			this.floorType = 4;
+		};
+
+		Tile.prototype.toMapExit = function() {
+			this.floorType = 5;
+		};
+
 		Tile.prototype.toFloor = function() {
 			this.floorType = 0;
+			this.connected = 0;
 		};
 
 		Tile.prototype.connect = function() {
@@ -94,6 +111,27 @@ angular.module('Dungeon-Generator-App')
 			var num_borders = 0;
 			num_borders	= (this.Top == null) + (this.Bottom == null) + (this.Right==null) + (this.Left==null);
 			return num_borders ==1;
+		};
+
+		Tile.prototype.isLeftBorder = function() {
+			
+			return (this.isBorder() && this.Left==null);
+			
+		};
+
+		Tile.prototype.isRightBorder = function() {
+			return (this.isBorder() && this.Right==null);
+			
+		};
+
+		Tile.prototype.isTopBorder = function() {
+			return (this.isBorder() && this.Top==null);
+			
+		};
+
+		Tile.prototype.isBottomBorder = function() {
+			return (this.isBorder() && this.Bottom==null);
+			
 		};
 
 		Tile.prototype.isCorner = function() {
@@ -179,7 +217,23 @@ angular.module('Dungeon-Generator-App')
 				}
 			}	
 		}; 
+		Tile.prototype.isConnectedRoom = function() {
+		//	console.log(this.connected && this.isRoom());
+			return (this.connected && this.isRoom());
+		};
 
+		Tile.prototype.isConnectedPath = function() {
+			return (this.connected && this.isPath());
+		};
+		
+		Tile.prototype.isunConnectedRoom = function() {
+			return (this.isRoom() && !this.connected);
+		};
+
+		Tile.prototype.isunConnectedPath = function() {
+			return (this.isPath() && !this.connected);
+		};
+		
 		Tile.prototype.isFreeNode = function() {
 			var floorNeighbor = 0;
 		    if(this.isBorder()){// if is border
@@ -218,12 +272,12 @@ angular.module('Dungeon-Generator-App')
 			var bottomtile = this.getBottom();
 			var lefttile = this.getLeft();
 			var righttile = this.getRight();
-			console.log("In possibleDirections function");
+		/*	console.log("In possibleDirections function");
 			console.log(JSON.stringify(toptile));
 			console.log(JSON.stringify(bottomtile));
 	        console.log(JSON.stringify(lefttile));
 			console.log(JSON.stringify(righttile));
-	
+	    */
 			if(toptile!= null && toptile.isDeadEnd() && toptile.isFloor()){
 				possibleDirections.push('T');
 			}
@@ -250,7 +304,7 @@ angular.module('Dungeon-Generator-App')
 					floor_map.push(tile);
 				}	
 			}
-			console.log(JSON.stringify(floor_map));
+		//	console.log(JSON.stringify(floor_map));
 
 			
 		}
@@ -262,7 +316,7 @@ angular.module('Dungeon-Generator-App')
 				var row = floor_map.slice(i*MAP_Y,(i+1)*MAP_Y);
 				map_object.rows.push({"data":row});
 			}
-			console.info((map_object));
+		//	console.info((map_object));
 			return map_object;
 		}
 		var map_print = function(){
@@ -358,12 +412,12 @@ angular.module('Dungeon-Generator-App')
 
 					var x = Math.floor(Math.random()*(max_x-min_x))+min_x;
 					var y = Math.floor(Math.random()*(max_y-min_y))+min_y;
-				    console.log("attempt = "+attempt+" x, y, width, length is "+x+", "+y+", "+width+" ,"+length);
+				//    console.log("attempt = "+attempt+" x, y, width, length is "+x+", "+y+", "+width+" ,"+length);
 					var room = new Room(x,y,width,length);
-					console.log(JSON.stringify(room));
+				//	  console.log(JSON.stringify(room));
 
 					if (room.isNotOverlap()){
-						console.log("isnotoverlap is true");
+				//		console.log("isnotoverlap is true");
 						room.placeRoom();
 						room_place_success = 1;
 						break;
@@ -395,7 +449,12 @@ angular.module('Dungeon-Generator-App')
 			this.connected == 1;
 			this.connectTiles();
 		};
-
+		Path.prototype.hasDeadEnd = function() {
+			var deadEndTile = this.tiles.find(function(x){
+				return (floor_map[x].isDeadEnd());
+			});
+			return (deadEndTile != null);
+		};
 		Path.prototype.generatePath = function() {
 			var tilestack = [];
 			var cur_tile;
@@ -407,30 +466,30 @@ angular.module('Dungeon-Generator-App')
 
 			if (start_node != null){
 				start_node.toPath();
-				console.log("start_node now is"+JSON.stringify(start_node));
+			//	console.log("start_node now is"+JSON.stringify(start_node));
 				this.tiles.push(start_node._id);
 				tilestack.push(start_node);
 				var next_tile;
 
 				while(tilestack[0]){
-					console.log("tilestack size now is"+tilestack.length);
+			//		console.log("tilestack size now is"+tilestack.length);
 					cur_tile = tilestack[tilestack.length-1];
-	                console.log("cur_tile is "+JSON.stringify(cur_tile));	
+	        //       console.log("cur_tile is "+JSON.stringify(cur_tile));	
 					//get the last tile, seek whether there is valid location to extend path
 					var growOptions = cur_tile.possibleDirections();
-					console.log("grow options are"+growOptions);
+			//		console.log("grow options are"+growOptions);
 					if (growOptions.length == 0){
 					// if no grow option, pop tile, go back to previous tile
 						tilestack.pop();
 						continue;
 					}else{
-						console.log("Looking for next direction");
+			//			console.log("Looking for next direction");
 					// pick random from available options
 						var next_dir_idx = Math.floor(Math.random()*(growOptions.length));	
 						var next_dir = growOptions[next_dir_idx];
 
-						console.log("next_dir_idx is "+ next_dir_idx);
-						console.log("next_dir is "+ next_dir);
+			//			console.log("next_dir_idx is "+ next_dir_idx);
+			//			console.log("next_dir is "+ next_dir);
 						
 						switch(next_dir){
 							case 'T':
@@ -451,15 +510,15 @@ angular.module('Dungeon-Generator-App')
 						next_tile.inPathNumber = paths.length;
 						this.tiles.push(next_tile._id);
 						tilestack.push(next_tile);
-						console.log("next_tile is "+JSON.stringify(next_tile));
+					//	console.log("next_tile is "+JSON.stringify(next_tile));
 						continue;
 					}
 				}
-				console.log("Hey, i am getting out of the loop");
+			//	console.log("Hey, i am getting out of the loop");
 			}//(if start_node!=null)
 			
 			paths.push(this);	
-			console.log("Hey, i am putting in a new path to my paths array");	
+		//	console.log("Hey, i am putting in a new path to my paths array");	
 
 		};
 
@@ -473,11 +532,11 @@ angular.module('Dungeon-Generator-App')
 
 		var generateAllPaths = function(){
 			while (existFreeNode()){
-				console.log("find one free node");
+		//		console.log("find one free node");
 				var path = new Path();
 				path.generatePath();	
 			}
-			console.log("paths size is "+paths.length);
+		//	console.log("paths size is "+paths.length);
 		};
 
 		var findEntrance = function(){
@@ -524,10 +583,10 @@ angular.module('Dungeon-Generator-App')
 				var entrance = findEntrance();
 				entrance.toEntrance();
 				var roomtile = entrance.findRoomOfEntrance();
-				console.log("roomtile is "+JSON.stringify(roomtile));
+			//	console.log("roomtile is "+JSON.stringify(roomtile));
 				var roomNumber = roomtile.inRoomNumber;
-				console.log("roomnumber is "+roomNumber);
-				console.log("room is "+JSON.stringify(rooms[roomNumber]));
+			//	console.log("roomnumber is "+roomNumber);
+			//	console.log("room is "+JSON.stringify(rooms[roomNumber]));
 				var thisroom = rooms[roomNumber];
 				thisroom.Connect();
 				entrance.connect();	
@@ -538,9 +597,69 @@ angular.module('Dungeon-Generator-App')
 				}
 			}
 		}
-		var Entrance = function Entrance(){
+		
+		var removeDeadEnds = function(){
+			//console.log("paths size is "+paths.length);
 
-		}
+			for (var i = 0; i<paths.length; i++){
+			//	console.log("i is "+i);
+				var cur_path = paths[i];
+				while(cur_path.hasDeadEnd()){
+			//		console.log("path tile length is "+cur_path.tiles.length);
+					if(cur_path.tiles.length >0){
+						for (var j = cur_path.tiles.length-1; j>=0; j--){
+			//				console.log("j is "+j);
+							var tileidx = cur_path.tiles[j];
+			//				console.log("tileidx is "+tileidx);
+							if(floor_map[tileidx].isDeadEnd() && floor_map[tileidx].isPath()){
+			//					console.log("find dead end, j ="+j);
+								floor_map[tileidx].toFloor();
+								cur_path.tiles.splice(j,1);
+							}
+						}
+					}
+				}
+			//	console.log("cur_path doesn't has deadend, path size is"+paths[i].tiles.length);
+				if(cur_path.tiles.length == 0){
+					paths.splice(i,1);
+				}
+				 
+			}
+		};
+
+		var AddEntAndExit = function(){
+			var leftbordertile = paths[0].tiles.find(function(x){
+				return floor_map[x].isLeftBorder();
+			});
+
+			var topbordertile = paths[0].tiles.find(function(x){
+				return floor_map[x].isTopBorder();
+			});
+
+			var rightbordertile = paths[0].tiles.find(function(x){
+				return floor_map[x].isRightBorder();
+			});
+
+			var bottombordertile = paths[0].tiles.find(function(x){
+				return floor_map[x].isBottomBorder();
+			});
+
+		//	console.log("find one left borader cell");
+		//	console.log(leftbordertile);
+			
+			if (leftbordertile != null){
+				floor_map[leftbordertile].toMapEntrance();
+			}else if(topbordertile != null){
+				floor_map[topbordertile].toMapEntrance();
+			}
+
+			if (rightbordertile != null){
+				floor_map[rightbordertile].toMapExit();
+			}else if(bottombordertile != null){
+				floor_map[bottombordertile].toMapExit();
+			}
+
+		};
 
 		this.floor_map = floor_map;
 		this.Tile = Tile;
@@ -550,5 +669,7 @@ angular.module('Dungeon-Generator-App')
 		this.generateRooms = generateRooms;
 		this.generateAllPaths = generateAllPaths;
 		this.connectMaze = connectMaze;
+		this.removeDeadEnds = removeDeadEnds;
+		this.AddEntAndExit = AddEntAndExit;
 })
 
